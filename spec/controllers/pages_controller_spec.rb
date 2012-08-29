ca42ea347712b3ecc70b1139bb13ebec83381a98
Igor <myxawork@gmail.com>
2012-08-29 11:43:34 +0400

    Done 3 part of tutorial

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
+    it "should have the right title" do
+    get 'home'
+    response.should have_selector("title", 
+             :content => 
+         "Ruby on Rails Tutorial Sample App | Home ")
+    end 
   end
 
   describe "GET 'contact'" do
@@ -15,5 +18,18 @@ describe PagesController do
       response.should be_success
     end
   end
+
+  describe "GET 'about' " do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+                  :content => 
+                    "Ruby on Rails Tutorial Sample App | About")
+    end
+  end 
 
 end
